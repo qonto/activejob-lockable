df7203b5f82e1e7fa39ba63e0049d5d8919d5372
Dmytro Zakharov <dmytro@qonto.eu>
2017-11-09 16:42:34 +0100

    override enqueue method instead of using around_enqueue callback

--- lib/activejob/lockable/lockable.rb
+++ lib/activejob/lockable/lockable.rb
@@ -5,49 +5,33 @@ module ActiveJob
     extend ActiveSupport::Concern
 
     module ClassMethods
-      def lock_for(lock_period)
-        @lock_period = lock_period
-      end
-
-      def lock_period
-        @lock_period
-      end
-
       def on_locked(action)
-        @on_locked = action
-      end
-
-      def on_locked_action
-        @on_locked
-      end
-
-      def set(options)
-        lock_for(options[:lock])
-        super(options)
+        self.on_locked_action = action
       end
     end
 
     included do
-      around_enqueue do |job, block|
-        tag_logger(job.class.name, job.job_id) do
-          if locked?
-            logger.info "job is locked, expires in #{locked_ttl} second"
-            send(on_locked_action) if on_locked_action && respond_to?(on_locked_action)
-          else
-            lock!
-            block.call
-          end
-          lock_for(nil) # resets instance variable to avoid further locks without any reason when set(lock: N) is used
+      attr_reader :options
+      class_attribute :on_locked_action
+
+      def enqueue(options = {})
+        @options = options
+        if locked?
+          logger.info "job is locked, expires in #{locked_ttl} second"
+          send(on_locked_action) if on_locked_action && respond_to?(on_locked_action)
+        else
+          lock!
+          super(options)
         end
       end
 
       def lock!
         return if lock_period.to_i <= 0
-        logger.info "locked with #{lock_key} for #{lock_period.to_i} seconds before lock! job_id: #{self.job_id} class_name: #{self.class}"
+        logger.info "locked with #{lock_key} for #{lock_period} seconds. Job_id: #{self.job_id} class_name: #{self.class}"
         begin
-          ActiveJob::Lockable::RedisStore.setex(lock_key, lock_period.to_i, self.job_id)
+          ActiveJob::Lockable::RedisStore.setex(lock_key, lock_period, self.job_id)
         rescue => e
-          logger.info "locked with #{lock_key} for #{lock_period.to_i} seconds after lock! job_id: #{self.job_id} class_name: #{self.class}"
+          logger.info "EXCEPTION: locked with #{lock_key} for #{lock_period} seconds. Job_id: #{self.job_id} class_name: #{self.class}"
           raise e
         end
       end
@@ -68,15 +52,7 @@ module ActiveJob
       private
 
       def lock_period
-        self.class.lock_period
-      end
-
-      def on_locked_action
-        self.class.on_locked_action
-      end
-
-      def lock_for(lock_period)
-        self.class.lock_for(lock_period)
+        options[:lock]
       end
     end
   end
